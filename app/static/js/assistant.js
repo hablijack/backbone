a825982b28b67ea7db172e58ebda734549270dae
habelc <christoph.habel@empiriecom.com>
2025-01-10 13:02:41 +0100

    feat: ux designworks

--- app/static/js/assistant.js
+++ app/static/js/assistant.js
@@ -1,21 +1,54 @@
-var Frontend = (function() {
+var Frontend = (function () {
+  var initClock = function () {
+    Frontend.updateClock();
+    setInterval("Frontend.updateClock()", 60000);
+  };
 
-    var initClock = function() {
-        Frontend.updateClock()
-        setInterval('Frontend.updateClock()', 60000);
-    };
-
-    return {
-        init: function() {
-            initClock();
+  var initZoeChart = function () {
+    var options = {
+      series: [67],
+      chart: {
+        height: 350,
+        type: "radialBar",
+        offsetY: 0,
+      },
+      colors: ['#73d8fd'],
+      plotOptions: {
+        radialBar: {
+          startAngle: -180,
+          endAngle: 180,
+          track: {
+            show: false,
+          },
+          legend: {
+            show: false,
+          },
         },
-
-        updateClock: function() {
-            $('.clock').html(moment().locale('de').format('LLL'));
-        }
+      },
+      stroke: {
+        dashArray: 15
+      },
     };
+
+    var chart = new ApexCharts(
+      document.querySelector("#zoe-fuel-chart"),
+      options
+    );
+    chart.render();
+  };
+
+  return {
+    init: function () {
+      initClock();
+      initZoeChart();
+    },
+
+    updateClock: function () {
+      $(".clock").html(moment().locale("de").format("LLL"));
+    },
+  };
 })();
 
-$(document).ready( function() {
-	Frontend.init();
+$(document).ready(function () {
+  Frontend.init();
 });
